fc973107cce702c9e29fc473a67ef9cc5171e0a7
Kundan <ntamsoy662gmail.com>
2023-05-19 08:53:05 +0530

    added signup modal and signin modal

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 let modalbtn = document.getElementById('modal')
 let searchBox = document.getElementById('search-box')
+let btnCreateClose = document.getElementById('btn-create-close')
+let btnSignInClose = document.getElementById('btn-signIn-close')
+let modalSignIn = document.getElementById('modalSignIn')
 function modal(){
     modalbtn.click();
 }
 function search(){
     searchBox.style.display = 'inline'
+}
+function signIn() {
+    btnCreateClose.click()
+    modalSignIn.click()
+}
+function createAccount() {
+    btnSignInClose.click()
+    modalbtn.click();
 }
